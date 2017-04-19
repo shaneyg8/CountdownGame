aedab1dbf96b14039f4b39d9d0135e2901538b21
shaneyg8 <shanegleeson22@gmail.com>
2017-04-19 11:58:00 +0100

    printing 1 and -1s

--- main.rkt
+++ main.rkt
@@ -1,4 +1,4 @@
-
+#lang racket
 (define start(list -1 -1 -1 -1 1 1 1 1))
 
 ;This line creates all permutations and it removes the duplicates 
@@ -15,8 +15,8 @@
 ;e= list
 (define (valid-rpn? e (s 0)) ;default value is 0
   (if (null? e)
-      (if =) s 1) #t #f)
-      (if =) (car e) 1)
+      (if (= s 1) #t #f)
+      (if (= (car e) 1)
           (valid-rpn? (cdr e) (+ s 1))
       (if (< s 2)
           #f
