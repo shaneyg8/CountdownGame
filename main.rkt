79fa628471dfa1c5515b138920b120e42e599bb3
shaneyg8 <shanegleeson22@gmail.com>
2017-04-19 11:54:51 +0100

    Taken from lectures

--- main.rkt
+++ main.rkt
@@ -1,61 +1,23 @@
-#lang racket
-;The main file for the numbers game
 
+(define start(list -1 -1 -1 -1 1 1 1 1))
 
-      ;Define the namespace
-(define ns (make-base-namespace))
+;This line creates all permutations and it removes the duplicates 
+(define perms (remove-duplicates (permutations start)))
 
+;This function adds the two 1s to the front of the list and the -1 to the end of the list 
+(define (to-rpn l)
+ (append (list 1 1) l (list -1)))
 
-      ;Initialise variables
-;List of the six numbers, not random for now
-(define l (list 1 2 3 4 5))
-;The numerical answer for the equations to calculate it
-(define answerNumber 20)
-;Define the possible equation list
-(define answerEquations (list))
-(define signList (list "+" "-" "*" "/" ))
+(map to-rpn perms)
 
-
-
-
-
-     ;Initial method for beginning the search
-(define(findAnswers originalList oAnswer )
-  
-  ;Outer for loop iterating through each number in list
-  (for([currentNumber originalList])
-    
-    ;Inner for loop applying each sign to the equation
-    (for ([sign signList])
-      
-      ;Define the current equation
-      (define currentEq (createEquation oAnswer sign currentNumber));
-
-      ;Calculate the current Equation
-      (define currentAnswer (eval (read (open-input-string currentEq)) ns))
-      
-      ;Display the calculation
-      (display (string-append (~v currentEq) " = "(~v currentAnswer) "\n") )
-      
-      ;Evaluate if answer is equal to the answer number
-      (if ( = currentAnswer answerNumber)
-          (display (string-append  " success" "\n" )) (printf (string-append " failure" "\n" ))) ;TODO return)
-      ;Evaluate if the answer is a negative number or a fraction
-      ;(if ((<= answer 0) or ((modulo answer 2) not 0) )
-      ;     (display "answer is uneven, return") (display"continue calculations"));TODO return
-     ; )
-      ;Evaulate whether the list is empy and if so call the same function recursively
-      ;TODO loop function
-      )
-  )
-)
-
-     ;Method for calculating the current equation
-(define (createEquation oAnswer sign currentNumber)
-  (string-append "( " sign " " (~v oAnswer)" " (~v currentNumber) " )")
-)
-
-;Call the main search method
-( findAnswers l 20 )
-
-
+;Apply function to every element of the list to get valid RPN
+;s= stack to see how many on it
+;e= list
+(define (valid-rpn? e (s 0)) ;default value is 0
+  (if (null? e)
+      (if =) s 1) #t #f)
+      (if =) (car e) 1)
+          (valid-rpn? (cdr e) (+ s 1))
+      (if (< s 2)
+          #f
+          (#t))))) ;true ot false
