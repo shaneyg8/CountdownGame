1676568cf706a40940efc4c0a88d5815df9c24a5
shaneyg8 <shanegleeson22@gmail.com>
2017-04-22 20:42:58 +0100

    Removing errors from code in class and starting it from fresh to be used with RPN

--- main.rkt
+++ main.rkt
@@ -12,7 +12,7 @@
 ;This function is taken from the link above
 (define (calculate-RPN expr)
   (for/fold ([stack '()]) ([token expr])
-    ;(printf "~a\t -> ~a~N" token stack) ; Uncomment to see workings, not recommended for long lists.
+    (printf "~a\t -> ~a~N" token stack) 
     (match* (token stack)
      [((? number? n) s) (cons n s)]
      [('+ (list x y s ___)) (cons (+ x y) s)]
@@ -26,25 +26,8 @@
      [(x s) (error "calculate-RPN: Cannot calculate the expression:" 
                    (reverse (cons x s)))])))
 
+
+;Removing errors to work on what we were given in class
 ;This line creates all permutations and it removes the duplicates 
-(define perms (remove-duplicates (permutations flatten)))
+;(define perms (remove-duplicates (permutations flatten)))
 
-;This function adds the two 1s to the front of the list and the -1 to the end of the list 
-(define (to-rpn l)
- (append (list 1 1) l (list -1)))
-
-(map to-rpn perms)
-
-;Apply function to every element of the list to get valid RPN
-s= stack to see how many on it
-e= list
-(define (valid-rpn? e (s 0)) ;default value is 0
-  (if (null? e)
-      (if (= s 1) #t #f)
-      (if (= (car e) 1)
-         (valid-rpn? (cdr e) (+ s 1))
-      (if (< s 2)
-          #f
-          (#t))))) ;true ot false
-
-;Calculate RPN
